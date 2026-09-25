f18c3777afc67067fdfecc6affe0e8a765deccdb
Nattapong-top <37808442+Nattapong-top@users.noreply.github.com>
2026-09-25 16:47:00 +0700

    feat(domain): updated Username value object with length and whitespace validations with unit tests

--- internal/domain/username.go
+++ internal/domain/username.go
@@ -1,1 +1,31 @@
 package domain
+
+import (
+	"errors"
+	"strings"
+	"unicode/utf8"
+)
+
+type Username struct {
+	value string
+}
+
+func NewUsername(value string) (Username, error) {
+	trimmedValue := strings.TrimSpace(value)
+
+	if trimmedValue == "" {
+		return Username{}, errors.New("กรุณากรอก Username และห้ามเป็นค่าว่างครับ")
+	}
+
+	if utf8.RuneCountInString(trimmedValue) < 6 {
+		return Username{}, errors.New("username ต้องมีความยาวอย่างน้อย 6 ตัวอักษร")
+	}
+
+	return Username{
+		value: trimmedValue,
+	}, nil
+}
+
+func (u Username) Value() string {
+	return u.value
+}
--- internal/domain/username_test.go
+++ internal/domain/username_test.go
@@ -1,1 +1,41 @@
 package domain
+
+import "testing"
+
+func TestNewUsername_Success(t *testing.T) {
+	username := "nattap"
+	usernameVo, err := NewUsername(username)
+
+	if err != nil {
+		t.Fatalf("คาดว่าไม่มี error แต่มี err: %v", err)
+	}
+
+	if usernameVo.Value() != username {
+		t.Errorf(
+			"คาดหวังให้ username %q, ได้รับ %q",
+			username,
+			usernameVo.Value(),
+		)
+	}
+}
+
+func TestNewUsername_entry(t *testing.T) {
+	username := ""
+	usernameVo, err := NewUsername(username)
+
+	if err == nil {
+		t.Fatal("คาดว่าจะเกิด error เมื่อ username ว่าง")
+	}
+
+	if usernameVo.Value() != "" {
+		t.Errorf("คาดว่า username จะว่าง แต่ได้ %q", usernameVo.Value())
+	}
+}
+
+func TestUsername_MinLen_LessThan6Chars(t *testing.T) {
+	username := "natta"
+	_, err := NewUsername(username)
+	if err == nil {
+		t.Fatal("คาดว่าจะเกิด error เมื่อความยาว username น้อยกว่า 6 ตัวอักษร")
+	}
+}
