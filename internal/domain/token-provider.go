6fdd2c247c8e7827b91c18b941a6aa78e9a07b5f
Nattapong-top <37808442+Nattapong-top@users.noreply.github.com>
2026-09-26 23:14:58 +0700

    feat(auth): implement JWT Auth Middleware and token claim validations

--- internal/domain/token-provider.go
+++ internal/domain/token-provider.go
@@ -1,5 +1,11 @@
 package domain
 
+type CustomClaims struct {
+	Username string
+	Hospital string
+}
+
 type TokenProvider interface {
 	GenerateToken(staff *Staff) (string, error)
+	ValidateToken(token string) (*CustomClaims, error)
 }
